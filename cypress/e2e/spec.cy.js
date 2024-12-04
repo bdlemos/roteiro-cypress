2c13a48eea4928a013832f548856baf723a4b560
bdlemos <bdutralemos@gmail.com>
2024-12-04 20:38:54 -0300

    Criando tres novos testes E2E: 'Verifica a não persistência das tarefas ao recarregar', 'Edita uma tarefa existente' e 'Testa clear completed'

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,61 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Testa clear completed', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('button.clear-completed')
+      .click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES'); 
+  });
+
+  it('Edita uma tarefa existente', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}');
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .dblclick();
+
+    cy.get('input.edit')
+      .first()
+      .clear()
+      .type('Trabalho Final de ES{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .first()
+      .should('have.text', 'Trabalho Final de ES');
+  });
+
+  it('Verifica a não persistência das tarefas ao recarregar', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.reload();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0)
+  });
+
 });
